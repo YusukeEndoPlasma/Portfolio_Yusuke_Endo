ab132306ddbf012f3c0d7b4ad55dc961185a8195
Yusuke <76081716+YU88888888888@users.noreply.github.com>
2022-12-13 23:01:33 +0900

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,39 @@
+<head>
+<title>TAG index Webサイト</title>
+
+<script type="text/javascript">
+<!--
+
+function keisan(){
+
+	// 設定開始
+
+	// 商品1
+	var price1 = document.form1.goods1.selectedIndex * 500; // 単価を設定
+	document.form1.field1.value = price1; // 小計を表示
+
+	// 商品2
+	var price2 = document.form1.goods2.selectedIndex * 1000; // 単価を設定
+	document.form1.field2.value = price2; // 小計を表示
+
+	// 商品3
+	var price3 = document.form1.goods3.selectedIndex * 3000; // 単価を設定
+	document.form1.field3.value = price3; // 小計を表示
+
+	// 合計を計算
+	var total = price1 + price2 + price3;
+
+	// 設定終了
+
+
+	document.form1.field_total.value = total; // 合計を表示
+
+}
+
+// --> 
+</script> 
+
+</head>
 <body>
 
 <form action="#" name="form1">
